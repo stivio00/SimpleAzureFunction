d8aea0f4097c936f17eff3a3decd0b5ad3f3bc70
Stephen Krol <krol.stephen@gmail.com>
2022-02-24 11:32:52 +0100

    second commit

--- HttpTrigger1.cs
+++ HttpTrigger1.cs
@@ -8,13 +8,13 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace SimpleAzureFunction
+namespace Company.Function
 {
-    public static class SimpleHttpAzureFunction
+    public static class HttpTrigger1
     {
-        [FunctionName("SimpleHttpAzureFunction")]
+        [FunctionName("HttpTrigger1")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
@@ -27,7 +27,7 @@ namespace SimpleAzureFunction
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                : $"Hello, {name}. This HTTP triggered function executed successfully!";
 
             return new OkObjectResult(responseMessage);
         }
